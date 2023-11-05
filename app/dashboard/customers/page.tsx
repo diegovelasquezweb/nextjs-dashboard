8c828b33c00ce311aaea0df389f27e279272b502
Diego Velasquez <contacto@diegovelasquez.co>
2023-11-05 18:13:41 -0500

    add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,28 @@
 import { Metadata } from 'next';
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import type { FormattedCustomersTable } from '@/app/lib/definitions';
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <main>
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        {/* @ts-ignore */}
+        <Table customers={customers} query={query} />
+      </Suspense>
+    </main>
+  );
 }
 
 export const metadata: Metadata = {
