e04386912804f73925e571554c4a12ebbd25ca1e
Diego Velasquez <contacto@diegovelasquez.co>
2023-11-05 22:04:34 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false,
-  images: {
-    unoptimized: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/dashboard',
-        permanent: true,
-      }
-    ];
-  }
 };
 
 module.exports = nextConfig;
