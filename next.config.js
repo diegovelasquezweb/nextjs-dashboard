02465dbddef8efda6b7f935aa7e5b9eff52f71dc
Diego Velasquez <contacto@diegovelasquez.co>
2023-11-06 18:43:47 -0500

    remove redirect

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/dashboard',
-        permanent: true,
-      }
-    ];
-  }
 };
 
 module.exports = nextConfig;
